2b0417cd2e735f25b705d0b43631eb8d623ed971
Jamie <jlhbond@gmail.com>
2017-06-26 00:59:55 -0400

    Add looping until exception, formatting.

--- xml_parser.py
+++ xml_parser.py
@@ -2,18 +2,35 @@ import xml.etree.ElementTree as ET
 import os
 import urllib2
 import time
+import sys
+import time
 
+url = 'http://web.mta.info/status/serviceStatus.txt'
 
+def get_status(xml_file, line_requested):
+	root = ET.parse(xml_file).getroot()
+	p_lines = []
+	p_status = []
 
+	for lines in root.findall('.//line'):
+		p_lines.append(lines.find('name').text)
+		p_status.append(lines.find('status').text)
 
-xml_file = urllib2.urlopen('http://web.mta.info/status/serviceStatus.txt')
-root = ET.parse(xml_file).getroot()
+	print p_lines[p_lines.index(line_requested)]
+	print p_status[p_lines.index(line_requested)]
+	print time.strftime("%m-%d-%y %H:%M:%S")
 
-p_lines = []
-p_status = []
-for lines in root.findall('.//line'):
-	p_lines.append(lines.find('name').text)
-	p_status.append(lines.find('status').text)
+	return;
 
-print p_lines[p_lines.index('ACE')]
-print p_status[p_lines.index('ACE')]
+def main(url):
+	try:
+		while True:
+			xml_loc = urllib2.urlopen(url)
+			get_status(xml_loc,'ACE')
+			time.sleep(5)
+	except KeyboardInterrupt:
+		print "You stopped it."
+		sys.exit(0)
+
+if __name__ == "__main__":
+	main(url)
